2df0c87c011420ff9dd7b719e4a0d6641fa88f07
shimaxu <shimax15@gmail.com>
2023-10-11 16:15:14 +0500

    build api for creating issue with validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import "easymde/dist/easymde.min.css";
-import React from "react";
+import React, { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
@@ -8,10 +8,16 @@ import { z } from "zod";
 import { IssueSchema } from "../IssueSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
+import { useRouter } from "next/navigation";
+import { Callout } from "@radix-ui/themes";
+import Spinner from "@/app/components/Spinner";
 
 type IssueForm = z.infer<typeof IssueSchema>;
 
 const IssueForm = () => {
+  const [error, setError] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+  const router = useRouter();
   const {
     register,
     control,
@@ -21,12 +27,34 @@ const IssueForm = () => {
     resolver: zodResolver(IssueSchema),
   });
 
-  const createIssue = handleSubmit((data) => {
-    console.log(data);
+  const createIssue = handleSubmit(async (data) => {
+    let response;
+    try {
+      setSubmitting(true);
+      response = await fetch("/api/issues", {
+        method: "post",
+        body: JSON.stringify(data),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    if (response?.ok) {
+      router.push("/issues");
+    } else {
+      setError("Unexpected error occured!");
+      setSubmitting(false);
+    }
   });
 
   return (
     <form className="max-w-lg m-5 space-y-4" onSubmit={createIssue}>
+      {error && (
+        <Callout.Root variant="surface" color="red">
+          <Callout.Text color="red">{error}</Callout.Text>
+        </Callout.Root>
+      )}
+
       <TextField.Root>
         <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
@@ -40,7 +68,10 @@ const IssueForm = () => {
       />
       <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
-      <Button type="submit">Submit Issue</Button>
+      <Button disabled={submitting} type="submit">
+        Submit Issue
+        {submitting && <Spinner />}
+      </Button>
     </form>
   );
 };
